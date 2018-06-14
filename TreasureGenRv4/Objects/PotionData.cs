561dabc2504e07f3a24cbb988fa3ccbda0c62655
BlakeXiaoLong <ajimenez3600@gmail.com>
2018-06-14 00:07:26 -0400

    Blew up everything to clear the way for new json classes

---
+++ TreasureGenRv4/Objects/PotionData.cs
@@ -0,0 +1,16 @@
+﻿namespace TreasureGenRv4.Objects
+{
+    internal class PotionData
+    {
+        public string LesserMinor { get; set; }
+        public string GreaterMinor { get; set; }
+        public string LesserMedium { get; set; }
+        public string GreaterMedium { get; set; }
+        public string LesserMajor { get; set; }
+        public string GreaterMajor { get; set; }
+        public string CasterLevel { get; set; }
+        public string SpellLevel { get; set; }
+        public Rarity Rarity { get; set; }
+        public SpellList Spells { get; set; }
+    }
+}
